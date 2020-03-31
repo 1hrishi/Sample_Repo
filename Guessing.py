fd1212b9a799a2a8845139f5c4421a050c5e577c
1hrishi <55714091+1hrishi@users.noreply.github.com>
2020-03-31 20:05:59 +0530

    Add files via upload

---
+++ Guessing.py
@@ -0,0 +1,20 @@
+import random
+def guessing():
+    n = random.randint(1,10)
+    chances = 0
+    guess = int(input("Guess the number: "))
+    while chances<5:
+        if n == guess:
+            print("you guessed it right")
+            break
+        elif n < guess:
+            print("Number too high, guess the number lower than ", guess)
+        else:
+            print("number too low, guess the number higher than ", guess)
+        guess = int(input("Guess again: "))
+        chances += 1
+
+    if not chances < 5:
+        print("you lost")
+if __name__ == "__main__":
+    guessing()
